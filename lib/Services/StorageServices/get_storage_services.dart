fd76bb71b442b18bb393d3043f11c236f1ba8d06
anasmufti97 <muftianas97@gmail.com>
2023-08-20 01:47:56 +0500

    code optimization

---
+++ lib/Services/StorageServices/get_storage_services.dart
@@ -0,0 +1,10 @@
+// import 'package:get_storage/get_storage.dart';
+//
+// class AppStorage {
+//   static String getUser = "getUser";
+//   static String intro = "intro";
+//
+//   static final box = GetStorage();
+//
+//   static String rememberMe = "rememberMe";
+// }
